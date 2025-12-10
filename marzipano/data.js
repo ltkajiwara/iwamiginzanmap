df64411bd67d285ff8e177a1565642cb1a5304b2
ltkajiwara <lt.kajiwara@gmail.com>
2025-12-11 03:39:30 +0900

    data.js

---
+++ marzipano/data.js
@@ -0,0 +1,7 @@
+// Describe your cylindrical panorama here
+
+const panoImage = {
+  url: "../acropolistest.jpg",   // Move image here OR adjust path
+  width: 7904,                   // Full pano width
+  height: 2304                   // Vertical resolution of your image
+};
